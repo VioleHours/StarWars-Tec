518a74e66f6b6df3891d425c7d4948ce77bbe948
Viole Hours <holasoyviole@gmail.com>
2024-05-14 13:05:54 -0300

    add(-): creating the button component

---
+++ client/components/Button.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+interface ButtonProps {
+  href: string;
+  imgSrc: string;
+  imgAlt: string;
+  imgFilter: string;
+  title: string;
+  gradientFrom: string;
+  gradientTo: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  href,
+  imgSrc,
+  imgAlt,
+  imgFilter,
+  title,
+  gradientFrom,
+  gradientTo,
+}) => (
+  <button className="border-white relative w-[15rem]">
+    <Link href={href}>
+      <div style={{ filter: imgFilter }}>
+        <img src={imgSrc} alt={imgAlt} />
+        <h3 className="font-bold text-2xl">
+          <span
+            className={`bg-gradient-to-r from-${gradientFrom} to-${gradientTo} bg-clip-text text-transparent`}
+          >
+            {title}
+          </span>
+        </h3>
+      </div>
+    </Link>
+  </button>
+);
