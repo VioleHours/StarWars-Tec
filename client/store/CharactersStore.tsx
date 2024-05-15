4c4a5ae41957825f7206c5f0e58c997b65080845
Viole Hours <holasoyviole@gmail.com>
2024-05-15 08:27:10 -0300

    fix(-): improvement in the store to bring all the characters with a scroll and obtain their ID based on their position in the array

--- client/store/CharactersStore.tsx
+++ client/store/CharactersStore.tsx
@@ -1,10 +1,20 @@
 import create from "zustand";
 import axios from "axios";
 
+function getCharacterId(url: string): string {
+  const segments = url.split("/");
+  const index = segments.findIndex((segment) => segment === "people");
+  if (index !== -1 && segments[index + 1]) {
+    return segments[index + 1];
+  }
+  return "";
+}
+
 interface Character {
   name: string;
   eye_color: string;
   gender: string;
+  url?: string;
 }
 
 interface CharacterStore {
@@ -21,6 +31,8 @@ interface CharacterStore {
   setSelectedEyeColor: (color: string) => void;
   setSelectedGender: (gender: string) => void;
   resetFilters: () => void;
+  characterCounter: number;
+  incrementCharacterCounter: () => void;
 }
 
 export const useCharacterStore = create<CharacterStore>((set) => ({
@@ -39,29 +51,36 @@ export const useCharacterStore = create<CharacterStore>((set) => ({
   resetFilters: () => {
     set({ selectedEyeColor: "", selectedGender: "" });
   },
+  characterCounter: 0,
+  incrementCharacterCounter: () =>
+    set((state) => ({ characterCounter: state.characterCounter + 1 })),
 }));
 
 export const fetchCharacters = async () => {
   try {
-    const response = await axios.get("https://swapi.dev/api/people/");
-    const characters: Character[] = response.data.results;
+    let response = await axios.get("https://swapi.dev/api/people/");
+    let characters: Character[] = response.data.results;
 
-    const filteredCharacters: Character[] = characters.map(
-      (character: any) => ({
-        name:
-          character.name !== "n/a" && character.name !== "unknown"
-            ? character.name
-            : "",
-        eye_color:
-          character.eye_color !== "n/a" && character.eye_color !== "unknown"
-            ? character.eye_color
-            : "",
-        gender:
-          character.gender !== "n/a" && character.gender !== "unknown"
-            ? character.gender
-            : "",
-      })
-    );
+    while (response.data.next) {
+      response = await axios.get(response.data.next);
+      characters = characters.concat(response.data.results);
+    }
+
+    const filteredCharacters: Character[] = characters.map((character) => ({
+      name:
+        character.name !== "n/a" && character.name !== "unknown"
+          ? character.name
+          : "",
+      eye_color:
+        character.eye_color !== "n/a" && character.eye_color !== "unknown"
+          ? character.eye_color
+          : "",
+      gender:
+        character.gender !== "n/a" && character.gender !== "unknown"
+          ? character.gender
+          : "",
+      url: getCharacterId(character.url ? character.url : ""),
+    }));
 
     const eyeColors: string[] = Array.from(
       new Set(filteredCharacters.map((char: Character) => char.eye_color))
